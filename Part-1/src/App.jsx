0c0d700ebcc4e62aca46ea5e02e23ff45f80d4ab
Pham Duong <phamduong11@gmail.com>
2024-05-26 15:40:21 +0700

    1.2: Course Information, step 2

--- Part-1/src/App.jsx
+++ Part-1/src/App.jsx
@@ -7,18 +7,22 @@ const Header = (props) => {
 };
 
 const Content = (props) => {
+    console.log(props);
     return (
         <>
-            <p>
-                {props.parts[0].name} {props.parts[0].exercises}
-            </p>
-            <p>
-                {props.parts[1].name} {props.parts[1].exercises}
-            </p>
-            <p>
-                {props.parts[2].name} {props.parts[2].exercises}
-            </p>
+            <Part part={props.parts[0]} />
+            <Part part={props.parts[1]} />
+            <Part part={props.parts[2]} />
         </>
+    );
+};
+
+const Part = (props) => {
+    console.log(props);
+    return (
+        <p>
+            {props.part.name} {props.part.exercises}
+        </p>
     );
 };
 
